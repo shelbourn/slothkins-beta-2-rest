30628a66c539c03964d5de20038eb1e55d7ee38e
Matthew Shelbourn <matthew@shelbourn.io>
2022-03-14 11:16:36 -0500

    Pre-deployment
    Fingers, toes, eyeballs, legs, arms, and everything else crossed!

---
+++ queries.js
@@ -0,0 +1,86 @@
+const Pool = require('pg').Pool;
+require('dotenv').config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const pool = new Pool({
+    connectionString: isProduction
+        ? process.env.DATABASE_URL
+        : connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
+};
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
+};
+
+const createUser = (request, response) => {
+    const { name, email } = request.body;
+
+    pool.query(
+        'INSERT INTO users (name, email) VALUES ($1, $2)',
+        [name, email],
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+            response.status(201).send(`User added with ID: ${result.insertId}`);
+        }
+    );
+};
+
+const updateUser = (request, response) => {
+    const id = parseInt(request.params.id);
+    const { name, email } = request.body;
+
+    pool.query(
+        'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+        [name, email, id],
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+            response.status(200).send(`User modified with ID: ${id}`);
+        }
+    );
+};
+
+const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).send(`User deleted with ID: ${id}`);
+    });
+};
+
+module.exports = {
+    pool,
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+};
