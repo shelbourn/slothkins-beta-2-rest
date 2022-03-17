7695b4eca01102fe3ca2037253a281cb8523e0e7
Matthew Shelbourn <matthew@shelbourn.io>
2022-03-16 19:26:52 -0500

    Adding cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const db = require('./queries');
 const port = 3000;
 
+app.use(cors);
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
