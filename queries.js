3591266cdb23cc887c20bf2b2e0acbee4bfb841c
Matthew Shelbourn <matthew@shelbourn.io>
2022-03-29 20:11:52 -0500

    Update queries.js

--- queries.js
+++ queries.js
@@ -105,7 +105,7 @@ const getAllCryptoPrices = (request, response) => {
     const currencyName = request.query.currencyName;
 
     pool.query(
-        `SELECT ARRAY(SELECT "${currencyName}" FROM "CurrencyPricesByDate")`,
+        `SELECT ARRAY(SELECT COALESCE("${currencyName}", 0) FROM "CurrencyPricesByDate")`,
         (error, results) => {
             if (error) {
                 throw error;
